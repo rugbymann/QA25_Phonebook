189eb59ad025cac859f17127b7405314214fff92
Anar Aliyev <rugbisto@gmail.com>
2023-09-03 23:07:13 +0200

    update

--- src/test/java/org/ait/phonebook/CreateAccountTests.java
+++ src/test/java/org/ait/phonebook/CreateAccountTests.java
@@ -1,13 +1,9 @@
 package org.ait.phonebook;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 public class CreateAccountTests extends TestBase{
 
@@ -15,32 +11,28 @@ public class CreateAccountTests extends TestBase{
 
 
     @BeforeMethod
-    public void ensurePrecondition(){
-        if(!isElementPresent(By.cssSelector("a:nth-child(4)"))){
-            driver.findElement(By.xpath("//button[contains(.,'Sign Out')]")).click();
+    public void ensurePrecondition(By clickOnSignButton){
+        if(!isLoginLinkPresent()){
+            clickOnSignOutButton();
         }
 
         //click on Login link  CSS - a:nth-child(4)
-        driver.findElement(By.cssSelector("a:nth-child(4)")).click();
+        clickOnLoginLink();
 
 
     }
 
     @Test
-    public void newUserRegistrationPositiveTest() {
+    public void existedUserRegistrationNegativeTest() {
         //enter email - [placeholder='Email'] - css
-        driver.findElement(By.cssSelector("[placeholder='Email']")).click();
-        driver.findElement(By.cssSelector("[placeholder='Email']")).clear();
-        driver.findElement(By.cssSelector("[placeholder='Email']")).sendKeys("orxan@mail.ru");
-        //enter password - [placeholder='Password'] - css
-        driver.findElement(By.cssSelector("[placeholder='Password']")).click();
-        driver.findElement(By.cssSelector("[placeholder='Password']")).clear();
-        driver.findElement(By.cssSelector("[placeholder='Password']")).sendKeys("Orxan123$");
+        fillLoginRegistrationForm("orxan@mail.ru", "Orxan123$");
         //click on Registration button - //button[text()='Registration'] - xpath
-        driver.findElement(By.xpath("//button[text()='Registration']")).click();
+        clickOnRegistrationButton();
         //assert Sign out button displayed - //button[contains(.,'Sign Out')] - xpath
-        Assert.assertTrue(isElementPresent2(By.xpath("//button[contains(.,'Sign Out')]")));
+        Assert.assertTrue((isAlertPresent()));
     }
+
+
 }
 
 
--- src/test/java/org/ait/phonebook/TestBase.java
+++ src/test/java/org/ait/phonebook/TestBase.java
@@ -1,8 +1,11 @@
 package org.ait.phonebook;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -13,33 +16,78 @@ public class TestBase {
     WebDriver driver;
 
     @BeforeMethod
-    public void setup(){
-        driver=new ChromeDriver();
+    public void setUp() {
+        driver = new ChromeDriver();
         driver.get("https://telranedu.web.app");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-    public boolean isHomeComponentPresent(){
-        return driver.findElements(By.xpath("//h1[text()='Home Component']")).size()>0;
-
+    public boolean isHomeComponentPresent() {
+        return driver.findElements(By.xpath("//h1[contains(.,'Home Component')]")).size() > 0;
     }
 
-    public boolean isElementPresent(By locator){
-        return driver.findElements(locator).size()>0;
+    public boolean isElementPresent(By locator) {
+        return driver.findElements(locator).size() > 0;
     }
 
-    public boolean isElementPresent2(By locator){
-        try{
-            driver.findElements(locator);
+    public boolean isElementPresent2(By locator) {
+        try {
+            driver.findElement(locator);
             return true;
-        }catch (NoSuchElementException ex){
+        } catch (NoSuchElementException ex) {
             return false;
         }
     }
 
-    @AfterMethod (enabled = false)
-    public void tearDown(){
+    @AfterMethod(enabled = false)
+    public void tearDown() {
         driver.quit();
+    }
+
+    protected void click(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    public boolean isAlertPresent() {
+        Alert alert = new WebDriverWait(driver, 20)
+                .until(ExpectedConditions.alertIsPresent());
+        if (alert == null) {
+            return false;
+        } else {
+            driver.switchTo().alert();
+            alert.accept();
+            return true;
+        }
+
+
+    }
+
+    public void clickOnLoginLink() {
+        click(By.cssSelector("a:nth-child(4)"));
+    }
+
+    public void clickOnSignOutButton() {
+        click(By.xpath("//button[text()='Login']"));
+    }
+
+    public boolean isLoginLinkPresent() {
+        return isElementPresent(By.cssSelector("a:nth-child(4)"));
+    }
+
+    public void clickOnRegistrationButton() {
+        click(By.xpath("//button[text()='Registration']"));
+    }
+
+    public void fillLoginRegistrationForm(String email, String password) {
+        type(By.cssSelector("[placeholder='Email']"), email);
+        //enter password - [placeholder='Password'] - css
+        type(By.cssSelector("[placeholder='Password']"), password);
     }
 }
